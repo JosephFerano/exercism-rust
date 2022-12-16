8c6d4e8bc4abac5ec806d8f581c134d369628d70
Joseph Ferano <joseph@ferano.io>
2022-12-16 17:25:02 +0700

    Make free standing functions methods

--- forth/src/lib.rs
+++ forth/src/lib.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::ops::{Add, Div, Mul, Sub};
 
 pub type Value = i32;
 pub type Result = std::result::Result<(), Error>;
@@ -18,63 +17,51 @@ pub enum Error {
     InvalidWord,
 }
 
-fn resolve(forth: &Forth, key: &String) -> Vec<String> {
-    match &forth.user_defined_words.get(key) {
-        Some(v) => v
-            .iter()
-            .map(|k| resolve(forth, k))
-            .collect::<Vec<Vec<String>>>()
-            .concat(),
-        None => vec![key.to_string()],
-    }
-}
-
-fn parse_word_definition(forth: &mut Forth, tokens: &mut [String]) -> Result {
-    let mut iter = tokens.iter();
-    let variable_name = iter.next().ok_or(Error::InvalidWord)?;
-    if let Some(c) = variable_name.chars().next() {
-        if !(c.is_alphabetic() || matches!(c, '+' | '-' | '*' | '/')) {
-            return Err(Error::InvalidWord);
-        }
-    } else {
-        return Err(Error::InvalidWord);
-    }
-    for (i, token) in iter.enumerate() {
-        let mut resolved = resolve(forth, token);
-        forth
-            .user_defined_words
-            .entry(variable_name.to_string())
-            .and_modify(|v| {
-                if i == 0 {
-                    v.clear();
-                }
-                v.append(&mut resolved);
-            })
-            .or_insert(resolved);
-    }
-    Ok(())
-}
-
-fn apply_arithmetic<F>(forth: &mut Forth, op: F) -> Result
-where
-    F: Fn(i32, i32) -> i32,
-{
-    match (forth.stack.pop(), forth.stack.pop()) {
-        (Some(rhs), Some(lhs)) => {
-            forth.stack.push(op(lhs, rhs));
-            Ok(())
-        }
-        _ => Err(Error::StackUnderflow),
-    }
-}
-
 impl Forth {
+
     pub fn new() -> Forth {
         Forth::default()
     }
 
     pub fn stack(&self) -> &[Value] {
         &self.stack
+    }
+
+    fn resolve(&self, key: &String) -> Vec<String> {
+        match self.user_defined_words.get(key) {
+            Some(v) => v
+                .iter()
+                .map(|k| self.resolve(k))
+                .collect::<Vec<Vec<String>>>()
+                .concat(),
+            None => vec![key.to_string()],
+        }
+    }
+
+    fn parse_word_definition(&mut self, tokens: &[String]) -> Result {
+        let mut iter = tokens.iter();
+        let variable_name = iter.next().ok_or(Error::InvalidWord)?;
+        if let Some(c) = variable_name.chars().next() {
+            if !(c.is_alphabetic() || matches!(c, '+' | '-' | '*' | '/')) {
+                return Err(Error::InvalidWord);
+            }
+        } else {
+            return Err(Error::InvalidWord);
+        }
+        for (i, token) in iter.enumerate() {
+            let mut resolved = self.resolve(token);
+            self
+                .user_defined_words
+                .entry(variable_name.to_string())
+                .and_modify(|v| {
+                    if i == 0 {
+                        v.clear();
+                    }
+                    v.append(&mut resolved);
+                })
+                .or_insert(resolved);
+        }
+        Ok(())
     }
 
     pub fn eval(&mut self, input: &str) -> Result {
@@ -97,13 +84,22 @@ impl Forth {
                         continue;
                     }
                 }
-                "+" => apply_arithmetic(self, Add::add)?,
-                "-" => apply_arithmetic(self, Sub::sub)?,
-                "*" => apply_arithmetic(self, Mul::mul)?,
-                "/" => match self.stack.last() {
-                    Some(0) => return Err(Error::DivisionByZero),
-                    Some(_) => apply_arithmetic(self, Div::div)?,
-                    None => return Err(Error::StackUnderflow),
+                "+" => match (self.stack.pop(), self.stack.pop()) {
+                    (Some(rhs), Some(lhs)) => self.stack.push(lhs + rhs),
+                    _ => return Err(Error::StackUnderflow),
+                },
+                "-" => match (self.stack.pop(), self.stack.pop()) {
+                    (Some(rhs), Some(lhs)) => self.stack.push(lhs - rhs),
+                    _ => return Err(Error::StackUnderflow),
+                },
+                "*" => match (self.stack.pop(), self.stack.pop()) {
+                    (Some(rhs), Some(lhs)) => self.stack.push(lhs * rhs),
+                    _ => return Err(Error::StackUnderflow),
+                },
+                "/" => match (self.stack.pop(), self.stack.pop()) {
+                    (Some(0), Some(_)) => return Err(Error::DivisionByZero),
+                    (Some(rhs), Some(lhs)) => self.stack.push(lhs / rhs),
+                    _ => return Err(Error::StackUnderflow),
                 },
                 "dup" => match self.stack.last() {
                     Some(n) => self.stack.push(*n),
@@ -123,14 +119,12 @@ impl Forth {
                     _ => return Err(Error::StackUnderflow),
                 },
                 ":" => {
-                    match tokens[1+cursor..].iter().position(|t| t == ";") {
+                    cursor += 1; // Skip semicolon
+                    match tokens[cursor..].iter().position(|t| t == ";") {
                         Some(index) => {
-                            // Get absolute position rather than the relative to the previous slice
-                            // If we don't do this the multiple definitions just find the first semicolon
-                            let index = cursor + index + 1;
-                            let start = cursor + 1;
-                            cursor = index + 1;
-                            parse_word_definition(self, &mut tokens[start..index])?;
+                            let result = self.parse_word_definition(&tokens[cursor..][..index]);
+                            cursor += index + 1;
+                            result?
                         }
                         None => return Err(Error::InvalidWord),
                     }
